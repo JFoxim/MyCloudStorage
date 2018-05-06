dfb5056ba854ca4209bc76745f83aabef2635479
ShangareevRR <renanew@mail.ru>
2018-05-06 23:30:22 +0500

    Начало работы над облачным хранилищем. Настройка Hibernate.

---
+++ server/src/main/java/dao/impl/UserDBDao.java
@@ -0,0 +1,19 @@
+package dao.impl;
+
+import dao.UserDao;
+import models.User;
+
+import java.util.List;
+
+public class UserDBDao implements UserDao {
+
+    @Override
+    public User getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
---
+++ server/src/main/java/models/User.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+
+    public User(String login, String password, String email) {
+        this.id = UUID.randomUUID().toString();
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
